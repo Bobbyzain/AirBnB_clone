2e4c86e0066801f10e02c7eff82e7d2b5755991a
Bobbyzain <abhameed419@gmail.com>
2024-01-13 18:30:52 -0800

    Creating the BaseModel Class

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This is the base model to our Airbnb console which will be designed to help with initialization, serialization and  deserialization of our future instances
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self):
+        """
+        To initial the class
+        """
+        self.id = str(uuid.uuid4())
+        
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+
+    def save(self):
+        """
+        To save the time and date the instance of the object was updated
+        """
+        self.updated_at = datetime.utcnow()
+
+    def to_dict(self):
+        """
+        Return
+            instance_dict (dict): a dictionary  containing all keys/values of __dict__ of the instance
+        """
+        instance_dict = self.__dict__.copy()
+        instance_dict["__class__"] = self.__class__.__name__
+        instance_dict["created_at"] = self.created_at.isoformat()
+        instance_dict["updated_at"] = self.updated_at.isoformat()
+
+        return instance_dict
+
+    def __str__(self):
+        """
+        The format of the string for output
+        """
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+This contains both the example test and the unit test for project three of the Airbnb project
+
+"""
+import unittest
 from models.base_model import BaseModel
+
 
 my_model = BaseModel()
 my_model.name = "My First Model"
@@ -12,3 +18,51 @@ print(my_model_json)
 print("JSON of my_model:")
 for key in my_model_json.keys():
     print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+class TestBaseModel(unittest.TestCase):
+    def test_init(self):
+        """
+        for testing the model's initialization procedures
+        """
+        my_model = BaseModel()
+        
+        self.assertIsNotNone(my_model.id)
+        self.assertIsNotNone(my_model.created_at)
+        self.assertIsNotNone(my_model.updated_at)
+
+    def test_save(self):
+        """
+        To see if the save method works appropriately
+        """
+        my_model = BaseModel()
+
+        start_updated_at = my_model.updated_at
+        current_updated_at = my_model.save()
+        self.assertNotEqual(start_updated_at, current_updated_at)
+
+    def test_to_dict(self):
+        """
+        to confim the proper fuctioning of the dictionary method
+        """
+        my_model = BaseModel()
+
+        my_model_dict = my_model.to_dict()
+        self.assertIsInstance(my_model_dict, dict)
+        
+        self.assertEqual(my_model_dict["__class__"], "BaseModel")
+        self.assertEqual(my_model_dict["id"], my_model.id)
+        self.assertEqual(my_model_dict["created_at"], my_model.created_at.isoformat())
+        self.assertEqual(my_model_dict["updated_at"], my_model.updated_at.isoformat())
+
+    def test_str(self):
+        """
+        to test for the string printing function
+        """
+        my_model = BaseModel()
+
+        self.assertTrue(str(my_model).startswith("[BaseModel]"))
+        self.assertIn(my_model.id, str(my_model))
+        self.assertIn(str(my_model.__dict__), str(my_model))
+
+if __name__ == "__main__":
+    unittest.main()
